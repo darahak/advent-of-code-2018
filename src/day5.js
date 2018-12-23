fd66aca90f9ee8b0a1f6bd4119dff6b446949d99
darahak <dara.hak@gmail.com>
2018-12-23 16:09:44 +0100

    Day 5, part 2

--- src/day5.js
+++ src/day5.js
@@ -1,38 +1,53 @@
 module.exports = {
   /** @param {string} input */
   part1(input) {
-    let chars = input.split('');
-
-    let i = 0;
-
-    while (i < chars.length) {
-      const cur = chars[i];
-
-      let destroyed = false;
-
-      if (isUpperCase(cur)) {
-        if (chars[i - 1] === cur.toLowerCase()) {
-          chars.splice(i - 1, 2);
-          destroyed = true;
-        } else if (chars[i + 1] === cur.toLowerCase()) {
-          chars.splice(i, 2);
-          destroyed = true;
-        }
-      }
-
-      if (destroyed) {
-        i = 0;
-      } else {
-        ++i;
-      }
-    }
-
-    return chars.length;
+    return createPolymer(input).length;
   },
 
   /** @param {string} input */
-  part2(input) {}
+  part2(input) {
+    const types = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+    let shortestPolymerLength = input.length;
+
+    types.forEach(type => {
+      const polymer = createPolymer(input.replace(new RegExp(`${type}`, 'gi'), ''));
+
+      if (polymer.length < shortestPolymerLength) {
+        shortestPolymerLength = polymer.length;
+      }
+    });
+
+    return shortestPolymerLength;
+  }
 };
+
+/** @param {string} input */
+function createPolymer(input) {
+  let chars = input.split('');
+
+  let i = 0;
+
+  while (i < chars.length) {
+    const cur = chars[i];
+
+    if (isUpperCase(cur)) {
+      if (chars[i - 1] === cur.toLowerCase()) {
+        chars.splice(i - 1, 2);
+        i = i > 2 ? i - 2 : 0;
+        continue;
+      } else if (chars[i + 1] === cur.toLowerCase()) {
+        chars.splice(i, 2);
+        i = i > 1 ? i - 1 : 0;
+        continue;
+      }
+    }
+
+    ++i;
+  }
+
+  return chars;
+}
 
 /** @param {string} char */
 function isUpperCase(char) {
--- src/day5.test.js
+++ src/day5.test.js
@@ -6,5 +6,5 @@ test('Day 5', () => {
   const line = input.split('\n')[0];
 
   expect(part1(line)).toBe(11264);
-  expect(part2(line)).toBe(0);
+  expect(part2(line)).toBe(4552);
 });
