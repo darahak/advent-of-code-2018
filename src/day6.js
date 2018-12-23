ae0e8f0775fac0cd8e14e7b4d6d14645fb4ced59
darahak <dara.hak@gmail.com>
2018-12-24 00:29:43 +0100

    Day 6, part 1 ongoing: bug fixes

--- src/day6.js
+++ src/day6.js
@@ -14,12 +14,30 @@
  */
 
 module.exports = {
-  /** @param {Array<string>} lines */
+  /**
+   * @param {Array<string>} lines
+   * @returns {number}
+   */
   part1(lines) {
     const locations = parseCoordinates(lines);
-    const grid = createGrid(locations);
+    const { view, width, height } = createGrid(locations);
 
-    // TODO: For each grid point, find the closest location(s).
+    for (let y = 0; y < height; ++y) {
+      for (let x = 0; x < width; ++x) {
+        const closestLocations = getIndexOfClosestLocations({ x, y }, locations);
+        const pos = y * width + x;
+
+        if (closestLocations.length > 1) {
+          view[pos] = '.';
+        } else if (closestLocations.length === 1) {
+          view[pos] = closestLocations[0].toString();
+        } else {
+          throw new Error('No closest location found');
+        }
+      }
+    }
+
+    dumpGrid(view, width, height);
   },
 
   /** @param {Array<string>} lines */
@@ -28,17 +46,56 @@ module.exports = {
   }
 };
 
+function dumpGrid(view, width, height) {
+  const viewCopy = Array.from(view);
+
+  for (let i = 0; i < viewCopy.length; ++i) {
+    viewCopy[i] = viewCopy[i].padStart(2, ' ');
+  }
+
+  for (let i = width - 1; i < width * height; i += width) {
+    viewCopy[i] += '\n';
+  }
+
+  const fs = require('fs');
+  const path = require('path');
+
+  fs.writeFileSync(path.resolve(__dirname, '..', 'out', 'grid.txt'), viewCopy.join(' '));
+}
+
 /**
  * @param {Point} here
  * @param {Array<Point>} locations
+ * @returns {Array<number>}
  */
 function getIndexOfClosestLocations(here, locations) {
-  // TODO
+  let shortestDistance = Number.MAX_VALUE;
+  let distances = [];
+
+  locations.forEach(loc => {
+    const distance = getDistance(here, loc);
+    distances.push(distance);
+
+    if (distance < shortestDistance) {
+      shortestDistance = distance;
+    }
+  });
+
+  let results = [];
+
+  distances.forEach((d, i) => {
+    if (d === shortestDistance) {
+      results.push(i);
+    }
+  });
+
+  return results;
 }
 
 /**
  * @param {Point} p1
  * @param {Point} p2
+ * @returns {number}
  * */
 function getDistance(p1, p2) {
   return Math.abs(p2.x - p1.x) + Math.abs(p2.y - p1.y);
@@ -52,14 +109,17 @@ function createGrid(locations) {
   const width = Math.max(...locations.map(p => p.x)) + 1;
   const height = Math.max(...locations.map(p => p.y)) + 1;
 
-  return {
-    view: new Array(width * height).fill(''),
-    width,
-    height
-  };
+  let view = new Array(width * height).fill('');
+
+  locations.forEach((loc, i) => (view[loc.y * width + loc.x] = i.toString()));
+
+  return { view, width, height };
 }
 
-/** @param {Array<string>} lines */
+/**
+ * @param {Array<string>} lines
+ * @returns {Array<Point>}
+ */
 function parseCoordinates(lines) {
   return lines.map(line => {
     const [x, y] = line.split(',').map(half => half.trim());
--- src/day6.test.js
+++ src/day6.test.js
@@ -2,7 +2,7 @@ const { getInput } = require('./utils');
 const { part1, part2 } = require('./day6');
 
 test('Day 6', () => {
-  const input = getInput('day6.txt');
+  const input = getInput('day6-mini.txt');
   const lines = input.split('\n').filter(line => line.length > 0);
 
   // FIXME: Insert solutions.
